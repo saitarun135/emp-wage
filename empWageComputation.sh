e89b34d680a8f3f3341c5d71ff54137146402320
saitarun135 <saitarun135@gm.com>
2021-02-27 11:17:35 +0530

    Added:uc2-wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
+#contsants
+EMP_RATE_PER_HR=20
+FULL_TIME_HRS=8
+#variable
 empCheck=$((RANDOM%2))
 isPresent=1;
 if [ $empCheck -eq $isPresent ]
-then
-echo "employee is present"
-else
-echo "employee is absent"
+ then
+	salary=$(( $EMP_RATE_HR*$FULL_TIME_HRS ))
+	echo $salary
+	else
+	salary=0;
+	echo $salary
 fi
