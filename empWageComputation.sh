cf9660b6017d76e4cf6e4fefee162311c4d43642
saitarun135 <saitarun135@gm.com>
2021-02-28 10:59:58 +0530

    added:employeeWage-case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,16 @@
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ]
-then
-	empHrs=0;
-elif [ $isPartTime -eq $randomCheck ];
-then 
-empHrs=4;
-else
-emphrs=0;
-fi
+empCheck=$((RANDOM%3));
+case $empCheck in
+	$isFullTime)
+			empHrs=8;
+		;;
+		$isPartTime)
+		empHrs=4;
+;;
+*)
+empHrs=0;
+;;
+esac
 salary=$(($empHrs*$empRatePerHr));
