3c0151c6a3f14df1fab9e765d254d0a4710637d4
saitarun135 <saitarun135@gm.com>
2021-02-28 17:39:03 +0530

    working _hours reached to a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,29 @@
 #!/bin/bash -x 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
-for ((day=1; day<=$numWorkingDays; day++))
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+totalEmphr=0;
+totalWorkingDays=0;
+function getWorkingHours() {
+case $1 in
+$IS_FULL_TIME)
+workHours=8;
+;;
+$IS_PART_TIME)
+workHours=4
+;;
+*)
+workHours=0;
+;;
+esac
+echo $workHours
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&  $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
- empCheck=$((RANDOM%3));
-	case $empCheck in
-		$isFullTime)
-			empHrs=8;
-		;;
-		$isPartTime)
-		empHrs=4;
-		;;
-		*)
-		empHrs=0;
-		;;
-		esac
-salary=$(($empHrs*$empRatePerHr));
-totalSalary=$(($totalSalary+$salary));
+((totalWorkingDays++))
+workhours="$( getWorkingHours $((RANDOM%3)) )"
+totalWorkHours=$((totalWorkHours+workHours))
 done
+totalSalary=$(($totalWorkHours * $EMP_RATE_PER_HR));
