09fac1b385ee9a175989cb5d9a9995536ecaaacb
saitarun135 <saitarun135@gm.com>
2021-02-28 11:40:23 +0530

    Add-wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,23 @@
-#!/bin/bash -x	
+#!/bin/bash -x 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
-	$isFullTime)
+numWorkingDays=20;
+for ((day=1; day<=$numWorkingDays; day++))
+do
+ empCheck=$((RANDOM%3));
+	case $empCheck in
+		$isFullTime)
 			empHrs=8;
 		;;
 		$isPartTime)
 		empHrs=4;
-;;
-*)
-empHrs=0;
-;;
-esac
+		;;
+		*)
+		empHrs=0;
+		;;
+		esac
 salary=$(($empHrs*$empRatePerHr));
+totalSalary=$(($totalSalary+$salary));
+done
